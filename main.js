e87c12d4a7e88454adfea4ccb2f7920e65ecfb24
guga2-6-4 <106117352+guga2-6-4@users.noreply.github.com>
2023-08-15 19:11:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name = "Raphael";
+var last_name = "Veiga";
+concat()
+console.log(concat);
+var a = 23;
+var b = 10;
+var c = 9;
+var d = 11;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+var add = a + b + c;
+console.log(add);
+console.log("Oi!")
